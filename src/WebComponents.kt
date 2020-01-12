bec97681922391ac4a5d80892b55edb18f8cd9c3
Anton Keks <anton@azib.net>
2020-01-12 22:08:44 +0200

    rename fields

--- src/WebComponents.kt
+++ src/WebComponents.kt
@@ -10,7 +10,7 @@ abstract class CustomTag(val tag: String) {
     @JsName("attributeChanged") open fun attributeChanged(name: String, oldVal: String, newVal: String) {}
 
     companion object {
-        private val wrapImpl = jsFunction("impl", jsCode = ES6_CLASS_ADAPTER) as (CustomTag) -> () -> dynamic
+        private val wrapImpl = jsFunction("impl", jsCode = ES6_CLASS_ADAPTER) as (impl: CustomTag) -> () -> dynamic
 
         fun define(vararg tags: CustomTag) {
             tags.forEach { tag ->
@@ -21,22 +21,22 @@ abstract class CustomTag(val tag: String) {
 }
 
 abstract class RenderableCustomTag(tag: String) : CustomTag(tag) {
-    protected lateinit var element: HTMLElement
-    protected lateinit var shadow: HTMLElement
+    protected lateinit var outer: HTMLElement
+    protected lateinit var inner: HTMLElement
 
     // language=html
     abstract fun render(): String
 
-    override fun init(el: HTMLElement) {
-        element = el
-        shadow = el.attachShadow(ShadowRootInit(ShadowRootMode.OPEN)).unsafeCast<HTMLElement>()
+    override fun init(element: HTMLElement) {
+        outer = element
+        inner = element.attachShadow(ShadowRootInit(ShadowRootMode.OPEN)).unsafeCast<HTMLElement>()
     }
 
-    override fun mounted() = doRender()
-    override fun attributeChanged(name: String, oldVal: String, newVal: String) = doRender()
+    override fun mounted() = rerender()
+    override fun attributeChanged(name: String, oldVal: String, newVal: String) = rerender()
 
-    protected open fun doRender() {
-        shadow.innerHTML = render()
+    protected open fun rerender() {
+        inner.innerHTML = render()
     }
 
     protected inner class attr(private val name: String) {
@@ -45,27 +45,27 @@ abstract class RenderableCustomTag(tag: String) : CustomTag(tag) {
         }
 
         operator fun getValue(thisRef: RenderableCustomTag?, prop: KProperty<*>) =
-          thisRef?.element?.attr(name)
+          thisRef?.outer?.attr(name)
 
         operator fun setValue(thisRef: RenderableCustomTag?, prop: KProperty<*>, value: String) =
-          thisRef?.element?.attr(name, value)
+          thisRef?.outer?.attr(name, value)
     }
 }
 
 abstract class BindableCustomTag(tag: String): RenderableCustomTag(tag) {
-    override fun init(el: HTMLElement) {
-        super.init(el)
-        shadow.on("change") { e ->
+    override fun init(element: HTMLElement) {
+        super.init(element)
+        inner.on("change") { e ->
             val input = e.target as? HTMLInputElement
             val bindProp = input?.attr("bind")
-            if (bindProp != null) element.setAttribute(bindProp, input.value)
+            if (bindProp != null) outer.setAttribute(bindProp, input.value)
         }
     }
 
-    override fun doRender() {
-        super.doRender()
-        shadow.findAll<HTMLInputElement>("[bind]").forEach { el ->
-            el.value = element.attr(el.attr("bind")!!)!!
+    override fun rerender() {
+        super.rerender()
+        inner.findAll<HTMLInputElement>("[bind]").forEach { el ->
+            el.value = outer.attr(el.attr("bind")!!)!!
         }
     }
 }
